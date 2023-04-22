12c7ae8f2da42a7fc005f3697dbaca4b7c317011
karan <morekaran236@gmail.com>
2023-04-22 13:47:30 +0530

    weather app using html css js

---
+++ index.js
@@ -0,0 +1,42 @@
+
+let image =document.querySelector(".image")
+
+document.getElementById('btn').addEventListener('click',checkWether)
+async function checkWether(){
+	let cityName= document.getElementById("cityName").value
+	if(cityName==""){
+		cityName="mumbai"
+	}
+	
+	
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b82713eea6msh443127867b5db3bp13a8dbjsnea2223735e36',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${cityName}&days=1`, options)
+	.then(response => response.json())
+	.then(response =>{
+		console.log(response)
+			document.querySelector('.city').innerHTML=cityName;
+			document.querySelector('.temp').innerHTML=response.current.temp_c+"°C ";
+			document.querySelector('.humidity').innerHTML=response.current.humidity;
+			document.querySelector('.wind_speed').innerHTML=response.current.wind_kph;
+			image.src= response.current.condition.icon
+		} )
+		.catch(err => console.error(err));	
+}
+checkWether()
+
+
+
+
+
+
+
+
+
+
